68f5a976912691df9ad7b610e52c845afba61587
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2022-11-12 18:44:28 +0300

    feat: add basic html layout and js extras

---
+++ songbird/script/language.js
@@ -0,0 +1,30 @@
+const language = [
+  [
+    {
+      section: 'Разминка',
+    },
+    {
+      section: 'Воробьиные',
+    },
+    {
+      section: 'Лестные птицы',
+    },
+    {
+      section: 'Хищные птицы',
+    },
+    {
+      section: 'Певчие птицы',
+    },
+    {
+      section: 'Морские птицы',
+    },
+  ],
+  [
+    {
+      startbutton: 'Начать игру',
+      nextLevel: 'Следующий уровень',
+      score: 'Счет',
+    }
+  ]
+]
+export default language;
